041c9ab1a737f143ed918dd324b8d649898110dd
SOJAN <66105552+sojanonelson@users.noreply.github.com>
2024-04-08 16:47:21 +0530

    Update ApiConstants .js

--- frontend/src/constants/ApiConstants .js
+++ frontend/src/constants/ApiConstants .js
@@ -1,8 +1,8 @@
 const BACKEND_API = {
   BASE_API_URL: "https://chordbeats.onrender.com/api",
   INSTRUMENT_SOUND: "http://localhost:4000/api/instrument",
-  LOGIN:'http://localhost:4000/api/user/login',
-  REGISTER:'http://localhost:4000/api/user/register'
+  LOGIN:'https://chordbeats.onrender.com/api/user/login',
+  REGISTER:'https://chordbeats.onrender.com/api/user/register'
 };
 
 export default BACKEND_API;
